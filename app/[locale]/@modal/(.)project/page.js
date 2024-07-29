87c483c0e6a1e978ce8d540377ebfda25d7a7d49
Justin Radomski <justin@justinradomski.me>
2024-07-29 11:13:05 -0500

    Added middleware for i18n

--- app/[locale]/@modal/(.)project/page.js
+++ app/[locale]/@modal/(.)project/page.js
@@ -4,12 +4,12 @@ import SampleContent from "@/components/SampleContent"
 import { useEffect } from "react"
 
 export default function Page() {
-    useEffect(() => {
-        console.log("Modal page is mounted.")
-        return () => {
-        console.log("Modal page is unmounted.")
-        }
-    }, [])
+  useEffect(() => {
+    console.log("Modal page is mounted.")
+    return () => {
+    console.log("Modal page is unmounted.")
+      }
+  }, [])
   return (
     <div className="fixed left-0 top-0 z-50 h-full w-full overflow-auto bg-white bg-opacity-70 backdrop-blur-md">
         <div className="flex flex-col items-center justify-center h-full">
--- app/[locale]/project/page.js
+++ app/[locale]/project/page.js
@@ -3,12 +3,12 @@ import SampleContent from "@/components/SampleContent"
 export default function Page() {
   return (
     <div className="fixed left-0 top-0 z-50 h-full w-full overflow-auto bg-white">
-        <div className="flex flex-col items-center justify-center h-full">
-            <div className="text-red-600">
-                You are on the route page.
-            </div>
-            <SampleContent />
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="text-red-600">
+          You are on the route page.
         </div>
+        <SampleContent />
+      </div>
     </div>
   )
 }
