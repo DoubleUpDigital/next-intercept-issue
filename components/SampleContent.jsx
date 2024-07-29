6a6245578f6b8e823e479061a8283d4b7853a966
Justin Radomski <justin@justinradomski.me>
2024-07-29 09:53:42 -0500

    Added test content

---
+++ components/SampleContent.jsx
@@ -0,0 +1,9 @@
+export default function SampleContent() {
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-black">Sample Content</h1>
+            <p className="text-black">This is a sample content.</p>
+        </div>
+    )
+}
